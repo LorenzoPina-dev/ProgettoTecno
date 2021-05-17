96482d44014a0861647af5232be619f33a2f387b
DadoSpina <82999240+DadoSpina@users.noreply.github.com>
2021-05-17 19:35:01 +0200

    Update file.js

--- datiJS/HTML%mortalità/file.js
+++ datiJS/HTML%mortalità/file.js
@@ -1,73 +1,69 @@
-function Calcola(){
+function Calcola() {
+    let a = "";
+    var percentualeTotale = 0;
+
     var ageGroup = document.getElementsByName("ageGroup")
-    var age = 0;
-    for(var i = 0; i < 5; i++){
-        if(ageGroup[i].checked){
-            age = ageGroup[i].value
+    for (var i = 0; i < 5; i++) {
+        if (ageGroup[i].checked) {
+            percentualeTotale += parseFloat(ageGroup[i].value);
         }
     }
-    var percentualeEta = 0
-    var iperTensione = document.getElementById("checkIpertensione").checked
-    var respirazione = document.getElementById("checkRespiratori").checked
-    var diabete = document.getElementById("checkDiabete").checked
-    var cardio = document.getElementById("checkCardio").checked
-    var percentualeTotale = 0
+    if (document.getElementsByName("sexGroup")[0].checked) {
+        percentualeTotale += parseFloat(document.getElementsByName("sexGroup")[0].value);
+    }
+    else if (document.getElementsByName("sexGroup")[1].checked) {
+        percentualeTotale += parseFloat(document.getElementsByName("sexGroup")[1].value);
+    }
+    let cancro = document.getElementById("checkCancro");
+    let iperTensione = document.getElementById("checkIpertensione");
+    let respirazione = document.getElementById("checkRespiratori");
+    let diabete = document.getElementById("checkDiabete");
+    let cardio = document.getElementById("checkCardio");
+    let percentualeTemp = 0;
+    let conta = 0;
 
-    //percentuale età
-    if(age == 0){
-        percentualeEta = 20
+    if (cancro.checked){
+        percentualeTemp += parseFloat(cancro.value);
+        conta++;
     }
-    else if(age == 1){
-        percentualeEta = 30
+    if (iperTensione.checked){
+        percentualeTemp += parseFloat(iperTensione.value);
+        conta++;
     }
-    else if(age == 2){
-        percentualeEta = 50
+    if (respirazione.checked){
+        percentualeTemp += parseFloat(respirazione.value);
+        conta++;
     }
-    else if(age == 3){
-        percentualeEta = 500
+    if (diabete.checked){
+        percentualeTemp += parseFloat(diabete.value);
+        conta++;
     }
-    else if(age == 4) {
-        percentualeEta = 1480
-    }
-
-    //percentuale genere
-    if(document.getElementById("radioMaschio").checked){
-        percentualeEta += 15
-    }
-    else if (document.getElementById("radioFemmina").checked) {
-        percentualeEta += 4
-    }
-
-    //percentuale controlli malattie
-    if (cardio == true) {
-        percentualeTotale += percentualeEta * 10.5
+    if (cardio.checked){
+        percentualeTemp += parseFloat(cardio.value);
+        conta++;
     }
 
-    if (diabete == true) {
-        percentualeTotale += percentualeEta *  7.3
-    }
-
-    if (respirazione == true) {
-        percentualeTotale += percentualeEta * 6.3
+    if (percentualeTemp > 0) {
+        percentualeTotale *= percentualeTemp;
     }
     
-    if (iperTensione == true) {
-        percentualeTotale += percentualeEta * 6
+    if (percentualeTotale >= 92) {
+        percentualeTotale = 92;
     }
 
-    alert("rischio mortalità del " + percentuale / 100 + "%")
-}
+    percentualeTotale *= 100;
+    percentualeTotale = Math.floor(percentualeTotale);
 
-function Reset(){
-    document.getElementById("radio19").checked = false;
-    document.getElementById("radio39").checked = false;
-    document.getElementById("radio59").checked = false;
-    document.getElementById("radio79").checked = false;
-    document.getElementById("radioOver").checked = false;
-    document.getElementById("radioMaschio").checked = false;
-    document.getElementById("radioFemmina").checked = false;
-    document.getElementById("checkIpertensione").checked = false;
-    document.getElementById("checkRespiratori").checked = false;
-    document.getElementById("checkDiabete").checked = false;
-    document.getElementById("checkCardio").checked = false;
+
+
+    a = "Rischio mortalità del " + percentualeTotale / 100 + "%";
+
+    if (percentualeTotale / 100 > 19) {
+        document.getElementById("divRisultato").innerHTML = '<div class="classeRisultatoSopra20"><p class="p2">' + a + '</p></div>';
+    }
+    else if (percentualeTotale / 100 > 4) {
+        document.getElementById("divRisultato").innerHTML = '<div class="classeRisultatoSopra5"><p class="p2">' + a + '</p></div>';
+    } else {
+        document.getElementById("divRisultato").innerHTML = '<div class="classeRisultatoSotto5"><p class="p2">' + a + '</p></div>';
+    }
 }
