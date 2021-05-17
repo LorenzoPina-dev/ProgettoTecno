037ecc55c09c663ee44e0d13dce0acb59f1032a3
DadoSpina <82999240+DadoSpina@users.noreply.github.com>
2021-05-17 16:49:26 +0200

    Update file.js

--- datiJS/HTML%mortalità/file.js
+++ datiJS/HTML%mortalità/file.js
@@ -6,39 +6,54 @@ function Calcola(){
             age = ageGroup[i].value
         }
     }
+    var percentualeEta = 0
     var iperTensione = document.getElementById("checkIpertensione").checked
     var respirazione = document.getElementById("checkRespiratori").checked
     var diabete = document.getElementById("checkDiabete").checked
     var cardio = document.getElementById("checkCardio").checked
-    var percentuale = 0
+    var percentualeTotale = 0
 
     //percentuale età
     if(age == 0){
-        percentuale = 20
+        percentualeEta = 20
     }
     else if(age == 1){
-        percentuale = 30
+        percentualeEta = 30
     }
     else if(age == 2){
-        percentuale = 50
+        percentualeEta = 50
     }
     else if(age == 3){
-        percentuale = 500
+        percentualeEta = 500
     }
-    else{
-        percentuale = 1480
+    else if(age == 4) {
+        percentualeEta = 1480
     }
 
     //percentuale genere
     if(document.getElementById("radioMaschio").checked){
-        percentuale += 15
+        percentualeEta += 15
     }
-    else{
-        percentuale += 4
+    else if (document.getElementById("radioFemmina").checked) {
+        percentualeEta += 4
     }
 
     //percentuale controlli malattie
-    //add code
+    if (cardio == true) {
+        percentualeTotale += percentualeEta * 10.5
+    }
+
+    if (diabete == true) {
+        percentualeTotale += percentualeEta *  7.3
+    }
+
+    if (respirazione == true) {
+        percentualeTotale += percentualeEta * 6.3
+    }
+    
+    if (iperTensione == true) {
+        percentualeTotale += percentualeEta * 6
+    }
 
     alert("rischio mortalità del " + percentuale / 100 + "%")
 }
